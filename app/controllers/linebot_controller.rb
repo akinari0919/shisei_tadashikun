0a072e8cefd9755034fe3d17b4ec6227d6e414ee
akinari <a.mizo0919@gmail.com>
2021-09-11 08:22:50 +0900

    返信内容を「ほなやるで〜」に変更

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -31,7 +31,7 @@ class LinebotController < ApplicationController
         when Line::Bot::Event::MessageType::Text
           message = {
             type: 'text',  
-            text: 'ok' #返信内容
+            text: 'ほなやるで〜' #返信内容
           }
           # 応答メッセージを送る
           client.reply_message(event['replyToken'], message)
