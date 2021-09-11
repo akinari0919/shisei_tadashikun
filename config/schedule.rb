10d7aec272f94a3881e19a6a9f154683dffa9c53
akinari <a.mizo0919@gmail.com>
2021-09-11 10:00:08 +0900

    1分を10分に修正

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,10 +1,15 @@
+# Rails.rootを使用するために必要。なぜなら、wheneverは読み込まれるときにrailsを起動する必要がある
 require File.expand_path(File.dirname(__FILE__) + "/environment")
 
+# cronを実行する環境変数
 rails_env = ENV['RAILS_ENV'] || :development
 
+# cronを実行する環境変数をセット
 set :environment, rails_env
+# cronのログの吐き出し場所。ここでエラー内容を確認する
 set :output, "#{Rails.root}/log/cron.log"
 
-every 1.minutes do 
+# 10分ごとに実行する
+every 10.minutes do 
   rake 'push_line:push_line_message_morning'
 end
